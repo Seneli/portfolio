376effd7f41f33da204c5504c6859f0447bf2ff8
seneli <seneli.sene@gmail.com>
2022-04-17 16:04:54 -0400

    added column background and renewed color scheme

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,6 @@
 import Menu from './Menu';
 import Typewriter from 'typewriter-effect';
-import ProfilePic from "../content/pfp.png";
+import ProfilePic from '../content/pfp.png';
 import '../styles/main.scss';
 
 const About = () => {
@@ -9,76 +9,123 @@ const About = () => {
       <body id="about-bg">
         <Menu />
 
-        <main id="home">
-          <h1 className="lg-heading">
-            About <span className="about">Me</span>
-          </h1>
-          <h2 className="sm-heading">
-            <Typewriter
-              className="typewriter"
-              options={{
-                strings: [
-                  'Learn a bit about me',
-                  'As you can tell I love cartoons',
-                  'I also love fluffy dogs and clouds',
-                ],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </h2>
-          <div className="about-info">
-            <img
-              src={ProfilePic}
-              alt="Seneli Seneviratne"
-              className="bio-image"
-            />
-            <div className="bio">
-              <h3 className="title about">Bio</h3>
-              <p className="text">
-                Hey everyone, my name is Seneli,{' '}
-                <span className="about">(pronounced "Seh-nuh-lee")</span>, and I
-                go by She/Her pronouns. I'm a Computer Systems Engineering
-                Student at Carleton University and I'm passionate about web
-                development and using tech for social good. In my spare time you
-                can find me reading, playing the piano, watching anime or
-                painting!
-              </p>
-            </div>
+        <main id="about-home">
+          <div className="column">
+            <h1 className="lg-heading">
+              About <span className="about about-color">Me</span>
+            </h1>
+            <h2 className="sm-heading">
+              <Typewriter
+                className="typewriter"
+                options={{
+                  strings: [
+                    'Learn a bit about me',
+                    'As you can tell I love cartoons',
+                    'I also love fluffy dogs and clouds'
+                  ],
+                  autoStart: true,
+                  loop: true
+                }}
+              />
+            </h2>
+            <div className="about-info">
+              <img
+                src={ProfilePic}
+                alt="Seneli Seneviratne"
+                className="bio-image"
+              />
+              <div className="bio">
+                <h3 className="title about-color">Bio</h3>
+                <p className="text">
+                  Hey everyone, my name is Seneli,{' '}
+                  <span className="about-sec-color">
+                    (pronounced "Seh-nuh-lee")
+                  </span>
+                  , and I go by <span className="about-sec-color">She/Her</span>{' '}
+                  pronouns. I'm a{' '}
+                  <span className="about-sec-color">
+                    Computer Engineering Student
+                  </span>{' '}
+                  at Carleton University and I'm passionate about web
+                  development and using tech for social good. In my spare time
+                  you can find me reading, playing the piano, watching anime or
+                  painting!
+                </p>
+              </div>
 
-            <div className="tab tab-1">
-              <h3 className="about">Professional</h3>
-              <p>
-                In the past two years I have completed internships at Bell
-                Canada and CDW Canada. In Summer 2022, I will be working at{' '}
-                <span className="about">Amazon as a SDE Intern</span>. To learn
-                more, check out my <a >resume</a>!
-              </p>
-            </div>
-            <div className="tab tab-2">
-              <h3 className="about">Technical</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Quaerat, quo porro eum ex debitis nobis magni aliquam explicabo
-                id aperiam.
-              </p>
-            </div>
-            <div className="tab tab-3">
-              <h3 className="about">Volunteering</h3>
-              <p>
-                I am a strong believer in using Tech as a force for good. I
-                volunteer with{' '}
-                <span className="about">
-                  CU-WiSE (Women in Science and Engineering Carleton){' '}
-                </span>{' '}
-                and <span className="about">CU-Blueprint </span>. WISE
-                <br />
-                <br />I am a strong believer in using Tech as a force for good.
-                In my spare time I like to volunteer with CU-WiSE (Women in
-                Science and Engineering Carleton) and CUBlueprint. to support
-                other women in STEM via affirmative action and peer support
-                programs.
-              </p>
+              <div className="tab tab-1">
+                <h3 className="about-color">Professional</h3>
+                <p>
+                  This summer I will be working at{' '}
+                  <span className="about-sec-color">
+                    Amazon as a SDE Intern
+                  </span>
+                  .
+                  <br />
+                  <br />
+                  In the past two years I have completed internships at{' '}
+                  <span className="about-sec-color">Bell </span>
+                  and <span className="about-sec-color">CDW Canada</span>.
+                  <br />
+                  <br />
+                  I've also been a <span className="about-sec-color">
+                    TA
+                  </span>{' '}
+                  and <span className="about-sec-color">PASS Facilitator</span>{' '}
+                  at Carleton University. I'm very passionate about making
+                  education more accessible for others.
+                  <br />
+                  <br />
+                  To learn more, check out my{' '}
+                  <a
+                    className="dark-hover"
+                    href="https://drive.google.com/file/d/1szJtQAY7jR46DOy1x0ukWuj78PR_ZGLi/view?usp=sharing"
+                  >
+                    <u className="about-sec-color">resume</u>
+                  </a>{' '}
+                  and
+                  <a
+                    className="dark-hover"
+                    href="https://drive.google.com/file/d/1szJtQAY7jR46DOy1x0ukWuj78PR_ZGLi/view?usp=sharing"
+                  >
+                    <u className="about-sec-color"> Linkedin</u>
+                  </a>
+                  !
+                </p>
+              </div>
+              <div className="tab tab-2">
+                <h3 className="about-color">Technical</h3>
+                <p>
+                  My main interests are{' '}
+                  <span className="about-sec-color">Web Development </span>
+                  and
+                  <span className="about-sec-color">Cloud Computing.</span>
+                  <br />
+                  To learn more, check out my{' '}
+                  <a className="dark-hover" href="/projects">
+                    <u className="about-sec-color">past projects</u>
+                  </a>
+                  !
+                </p>
+              </div>
+              <div className="tab tab-3">
+                <h3 className="about-color">Volunteering</h3>
+                <p>
+                  I am a strong believer in using Tech as a force for good. I
+                  volunteer with{' '}
+                  <span className="about-sec-color">
+                    CU-WiSE (Women in Science and Engineering Carleton){' '}
+                  </span>{' '}
+                  and <span className="about-sec-color">CU-Blueprint </span>.
+                  WISE
+                  <br />
+                  <br />I am a strong believer in using Tech as a force for
+                  good. In my spare time I like to volunteer with CU-WiSE (Women
+                  in Science and Engineering Carleton) and CUBlueprint. to
+                  support other women in STEM via affirmative action and peer
+                  support programs.
+                </p>
+              </div>
             </div>
           </div>
         </main>
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -48,79 +48,81 @@ const Projects = () => {
         <Menu />
 
         <main id="home">
-          <h1 className="lg-heading">
-            My <span className="main-text-secondary">Projects</span>
-          </h1>
-          <h2 className="sm-heading">
-            <Typewriter
-              className="typewriter"
-              options={{
-                strings: [
-                  'Projects and technical skills',
-                  'Click the following skills to see where I used them',
-                ],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </h2>
+          <div className="proj column">
+            <h1 className="lg-heading">
+              My <span className="proj-sec-color">Projects</span>
+            </h1>
+            <h2 className="sm-heading">
+              <Typewriter
+                className="typewriter"
+                options={{
+                  strings: [
+                    'Projects and technical skills',
+                    'Click the following skills to see where I used them'
+                  ],
+                  autoStart: true,
+                  loop: true
+                }}
+              />
+            </h2>
 
-          <div className="skills">
-            {skills.map((skill, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`skill ${skill.area} ${skill.selected}`}
-                  onClick={() => {
-                    toggleSelected(index);
-                  }}
-                >
-                  {skill.name}
-                </div>
-              );
-            })}
-            <div className={`skill all-skills`} onClick={clearSelected}>
-              Clear Selection
-            </div>
-            <div className={`skill all-skills`} onClick={selectAll}>
-              Select All
-            </div>
-          </div>
-
-          <div className="projects">
-            {projects.map((project, index) => {
-              return (
-                isProjectVisible(project.skills) && (
-                  <div key={index} className="project">
-                    <h3 className="title">{project.name}</h3>
-
-                    <div className="skills">
-                      {project.skills.map((skill, index) => {
-                        return (
-                          <p key={index} className={`skill ${skill}`}>
-                            {skill}
-                          </p>
-                        );
-                      })}
-                    </div>
-                    <p className="description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Impedit, modi iste. Nam, qui nisi recusandae error sint
-                      vel asperiores tempore. {project.description}
-                    </p>
-                    <img
-                      className="image"
-                      src="https://pyxis.nymag.com/v1/imgs/168/564/d268eaf1cb956c02eba2edf728663887ce-19-bojack-todd.rsquare.w700.jpg"
-                      // {project.media}
-                      alt="project media"
-                    />
-                    <div className="github">
-                      <FaGithub />
-                    </div>
+            <div className="skills">
+              {skills.map((skill, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={`skill ${skill.area} ${skill.selected}`}
+                    onClick={() => {
+                      toggleSelected(index);
+                    }}
+                  >
+                    {skill.name}
                   </div>
-                )
-              );
-            })}
+                );
+              })}
+              <div className={`skill all-skills`} onClick={clearSelected}>
+                Clear Selection
+              </div>
+              <div className={`skill all-skills`} onClick={selectAll}>
+                Select All
+              </div>
+            </div>
+
+            <div className="projects">
+              {projects.map((project, index) => {
+                return (
+                  isProjectVisible(project.skills) && (
+                    <div key={index} className="project">
+                      <h3 className="title">{project.name}</h3>
+
+                      <div className="skills">
+                        {project.skills.map((skill, index) => {
+                          return (
+                            <p key={index} className={`skill ${skill}`}>
+                              {skill}
+                            </p>
+                          );
+                        })}
+                      </div>
+                      <p className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Impedit, modi iste. Nam, qui nisi recusandae error sint
+                        vel asperiores tempore. {project.description}
+                      </p>
+                      <img
+                        className="image"
+                        src="https://pyxis.nymag.com/v1/imgs/168/564/d268eaf1cb956c02eba2edf728663887ce-19-bojack-todd.rsquare.w700.jpg"
+                        // {project.media}
+                        alt="project media"
+                      />
+                      <div className="github">
+                        <FaGithub />
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+            </div>
           </div>
         </main>
         <footer>Made with Love ❤️ By Seneli</footer>
