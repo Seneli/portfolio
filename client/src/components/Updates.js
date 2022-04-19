4529ece90b5de27d25cf65924772a72275abf62e
seneli <seneli.sene@gmail.com>
2022-04-19 16:10:01 -0400

    deleted unused images + added default about page

--- client/src/components/Updates.js
+++ client/src/components/Updates.js
@@ -1,7 +1,31 @@
+import Typewriter from 'typewriter-effect';
+import Menu from './Menu';
+import '../styles/main.scss';
+
 const Updates = () => {
   return (
     <>
-      <div>Updates page</div>
+      <div id="updates-bg">
+        <Menu />
+        <main id="home">
+          <h1 className="lg-heading">
+            <Typewriter
+              className="typewriter"
+              options={{
+                strings: [
+                  'This page is coming soon',
+                  "I hope you have a good day!",
+                  "😁 ✌️"
+                ],
+                autoStart: true,
+                loop: true
+              }}
+            />
+          </h1>
+        </main>
+
+        <footer>Made with Love ❤️ By Seneli</footer>
+      </div>
     </>
   );
 };
